b69d0af1832d11376363f175f34420a304e36c13
Nikita <39242713+Nais-Alkator@users.noreply.github.com>
2022-08-10 02:12:46 +0300

    Settings have been updated.

--- proxy/settings.py
+++ proxy/settings.py
@@ -8,7 +8,7 @@ env.read_env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', '0.0.0.0', 'localhost'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
